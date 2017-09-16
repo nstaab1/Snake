b695b3a19c28874606df6c3fc863e908fb0cebf0
Nathan Staab <nathanstaab@addictent.com>
2017-09-15 20:38:04 -0400

    Added timer 9.15.17 8.30PM

--- Grid Design/Grid Design/Classes/SnakeBody.cs
+++ Grid Design/Grid Design/Classes/SnakeBody.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace Grid_Design.Classes
 {
-    class SnakeBody
+    public class SnakeBody
     {
+        private int posX;
+        private int posY;
+        private int snakePart;
+
+        public int PosX
+        {
+            get { return this.posX; }
+        }
+
+       
+        public int PosY
+        {
+            get { return this.posY; }
+        }
+
+        public int SnakePart
+        {
+            get
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                return this.snakePart;                
+            }
+        }
+
+        public SnakeBody (int posX, int posY, int snakePart)
+        {
+            this.posX = posX;
+            this.posY = posY;
+            this.snakePart = snakePart;
+        }
+
     }
 }
